9d662441e9b84b3b4d1c2213fd545ec65e77c0e1
Juan Antonio Jurado <juanantoniojurado@hotmail.es>
2019-05-12 19:23:09 +0200

    Added cost function

--- GA.py
+++ GA.py
@@ -12,10 +12,16 @@ class GeneticAlgorithm:
         assert(self.n > m), "The sample must be smaller than the data!"
         assert(m%2 == 0), "The sample size must be even!"
         
+        if(not isinstance(weightMatrix,np.matrix)): weightMatrix = np.matrix(weightMatrix)
+
         self.pop = np.ndarray((popSize,m))
 
         if initMethod == 'random':
             for i in range(popSize):
-                ind = np.full(self.n,False)
-                ind[np.random.choice(self.n,size=m,replace=False)] = True
+                ind = np.full(self.n,0)
+                ind[np.random.choice(self.n,size=m,replace=False)] = 1
                 self.pop[i] = ind
+    
+    def costFunction(self, sample, matrix):
+        if len(sample.shape) == 1: sample = sample[np.newaxis]
+        dist = np.float_(sample * matrix * sample.T)/2
