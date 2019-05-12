34dc6f888dd1bbb979d62fff4106ee476593817f
Juan Antonio Jurado <juanantoniojurado@hotmail.es>
2019-05-12 18:17:27 +0200

    Fix

--- GA.py
+++ GA.py
@@ -1,27 +1,21 @@
 import numpy as np
+import sys
 
-class GeneticAlgorithm():
+class GeneticAlgorithm:
 
     def __init__(self, weightMatrix, m, initMethod = 'random', popSize = 500 ):
-        if weightMatrix.shape[0] != weightMatrix.shape[1]: 
-            print("Matrix must be squared!")
-            return
-        if n > m:
-            print("The sample must be smaller than the data!")
-            return
-        if m%2 != 0:
-            print("The sample size must be even!")
-            return
-        
         self.matrix = weightMatrix
         self.m = m
         self.n = weightMatrix.shape[0]
+
+        assert(weightMatrix.shape[0] == weightMatrix.shape[1]), "Matrix must be squared!"
+        assert(self.n > m), "The sample must be smaller than the data!"
+        assert(m%2 == 0), "The sample size must be even!"
+        
         self.pop = np.ndarray((popSize,m))
 
         if initMethod == 'random':
             for i in range(popSize):
-                ind = np.full(n,False)
-                ind[np.random.choice(n,size=m,replace=False)] = True
-                pop[i] = ind
-
-        return self
+                ind = np.full(self.n,False)
+                ind[np.random.choice(self.n,size=m,replace=False)] = True
+                self.pop[i] = ind
