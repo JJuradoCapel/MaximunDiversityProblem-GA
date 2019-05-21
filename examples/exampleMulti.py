9389ba4d4bf3581ec3abc7b33ba219154711291f
Juan Antonio Jurado Capel <juanantoniojurado@hotmail.es>
2019-05-21 20:57:19 +0200

    reorder

--- examples/exampleMulti.py
+++ examples/exampleMulti.py
@@ -10,11 +10,10 @@ dataFiles = os.listdir("data")
 for table in dataFiles:
     results = []
     n,m,matrix = tableReader("data/" + table)
-    alg = GA(matrix,m,verbose=1)
     for seed in range(1000,1021):
         np.random.seed(seed)
+        alg = GA(matrix,m,verbose=1)
         _,cost,time = alg.run(timer=True)
-        alg.reset()
         results.append([cost, time])
 
     with open("results/" + table, 'w') as f:
